2f790243ced8d73579d752f642ff6a13a5b7b526
Matheus Costa <matheusoliveiracos@gmail.com>
2016-11-02 15:08:22 -0300

    add variables.sass and fixing editor height

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('default', function () {
-  gulp.src('./src/sass/*.sass')
+  gulp.src('./src/sass/main.sass')
       .pipe(sass({
         outputStyle: 'expanded'
       }).on('error', sass.logError))
